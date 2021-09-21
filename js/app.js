35ce89d6bc17eee23dd4a2b414d2761fe4c68d85
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-09-21 12:06:58 +0100

    Fix ESLint errors

--- js/app.js
+++ js/app.js
@@ -7,14 +7,14 @@ const authorInput = formEl.author;
 Book.displayBooks();
 
 formEl.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const title = titleInput.value;
-    const author = authorInput.value;
-    if (title.length > 0 || author.length > 0) {
-        const book = new Book(title, author);
-        Book.addBook(book);
-        Book.displayBooks();
-        titleInput.value = '';
-        authorInput.value = '';
-    }
+  e.preventDefault();
+  const title = titleInput.value;
+  const author = authorInput.value;
+  if (title.length > 0 || author.length > 0) {
+    const book = new Book(title, author);
+    Book.addBook(book);
+    Book.displayBooks();
+    titleInput.value = '';
+    authorInput.value = '';
+  }
 });
