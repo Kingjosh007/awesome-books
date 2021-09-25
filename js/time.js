6a9483a413434c7c5db49cffe0680e36e9eeeab8
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-09-25 01:15:22 +0100

    Fix Linters errors

--- js/time.js
+++ js/time.js
@@ -1,17 +1,17 @@
-const DateTime = luxon.DateTime;
+// eslint-disable-next-line no-undef
+const { DateTime } = luxon;
 
 const getTimeNow = () => DateTime.now();
 const getDateFormated = (gtn) => gtn.toFormat('MMMM dd, yyyy');
-const getHoursFormated = (gtn) => [gtn.toObject().hour, gtn.toObject().minute, gtn.toObject().second]
-                                  .map(el => el < 10 ? '0'+el: el)
-                                  .join(':');
+const getHoursRaw = (gtn) => [gtn.toObject().hour, gtn.toObject().minute, gtn.toObject().second];
+const getHoursFormatted = (hr) => hr.map((el) => (el < 10 ? `0${el}` : el)).join(':');
 
-let timeEl = document.querySelector('#luxon-time');
+const timeEl = document.querySelector('#luxon-time');
 
 setInterval(() => {
-    const tn = getTimeNow(),
-          df = getDateFormated(tn),
-          hf = getHoursFormated(tn);
-    timeEl.textContent = df + ', ' + hf;
-}, 1000)
-
+  const tn = getTimeNow();
+  const df = getDateFormated(tn);
+  const hr = getHoursRaw(tn);
+  const hf = getHoursFormatted(hr);
+  timeEl.textContent = `${df}, ${hf}`;
+}, 1000);
